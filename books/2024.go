d661086208d635df03d8a33e60e99f920b9e6b6a
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-02-13 23:58:21 +0800

    chore: remove Go code

--- books/2024.go
+++
@@ -1,9 +0,0 @@
-package books
-
-import "github.com/ytx-readings/reading-lists/types"
-
-var Books2024 = &types.BooksOfYear{
-	Book_RZJX,
-	Book_RZJX_Q,
-	Book_RZQD,
-}
--- books/by-year.go
+++
@@ -1,7 +0,0 @@
-package books
-
-import "github.com/ytx-readings/reading-lists/types"
-
-var AllBooksByYear = &types.BooksByYearMap{
-	2024: Books2024,
-}
